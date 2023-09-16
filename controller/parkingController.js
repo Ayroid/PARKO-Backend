3e6def2acbc7ed243da89722997d8abf8b53e18e
Vidhant007 <thapavidhant@gmail.com>
2023-09-16 14:48:40 +0530

    added routes/controllers for parking

--- controller/parkingController.js
+++ controller/parkingController.js
@@ -1,3 +1,4 @@
+// using same parking route for get all parkings and get specific parking with query
 const getParkingSpots = (req,res)=>{
     res.send('get parking spots')
 }
@@ -5,4 +6,22 @@ const getParkingSpots = (req,res)=>{
 const bookParkingSpots = (req,res)=>{
     res.send('book parking spot');
 }
+
+const raiseIssue = (req,res)=>{
+    res.send('Raise Issue');
+}
+
+const findCar = (req,res) =>{
+    res.send('find car');
+}
+
+module.exports = {
+    GETPARKING : getParkingSpots,
+    BOOKPARKING : bookParkingSpots,
+    RAISEISSUE : raiseIssue,
+    FINDCAR : findCar,
+}
+
+
+
 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,16 +7,31 @@ const {
   REGISTERVEHICLE,
 } = require("../controller/userController");
 
+const {
+  GETPARKING,
+  FINDCAR,
+  BOOKPARKING,
+  RAISEISSUE,
+} = require("../controller/parkingController");
+
 // ----------------- CREATING ROUTER -----------------
 const USER = express.Router();
 
 // ----------------- IMPORTING CONTROLLERS -----------------
 
 // ----------------- API ENDPOINTS -----------------
+
+//User routes
 USER.post("/register", REGISTER);
 USER.post("/login", LOGIN);
 USER.post("/userDetails", GETUSERDETAILS);
 USER.post("/registerVehicle", REGISTERVEHICLE);
+
+//parking Routes
+USER.post("/raise-issue", RAISEISSUE);
+USER.post("/findcar", FINDCAR);
+USER.post("/bookparking", BOOKPARKING);
+USER.post("/getparking", GETPARKING);
 
 // ----------------- EXPORTING ROUTER -----------------
 
