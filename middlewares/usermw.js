c3e533348ccd4adc3b2eb79024de0b6556f180ab
ayroid <ayushsk0000@gmail.com>
2023-09-28 10:55:14 +0530

    user middleware updated

---
+++ middlewares/usermw.js
@@ -0,0 +1,24 @@
+// USER DETAILS VERIFICATION CONTROLLER
+
+const verifyUser = (req, res, next) => {
+  const { username, phone, email, sapid } = req.body;
+  if (!username || !email || !phone || !sapid) {
+    return res.status(400).send("Mandatory fields cannot be empty!");
+  }
+  if (!phone.length === 10) {
+    return res.status(400).send("Invalid Phone Number!");
+  }
+  if (!sapid.length === 9) {
+    return res.status(400).send("Invalid SAPID!");
+  }
+  if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+    return res.status(400).send("Invalid Email!");
+  }
+  next();
+};
+
+// EXPORTING MODULES
+
+module.exports = {
+  VERIFYUSER: verifyUser,
+};
