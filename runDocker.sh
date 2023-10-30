2ae4f25857bfb4fc711cf3ef0ebf04a21d303820
Ayroid <ayushsk0000@gmail.com>
2023-10-30 19:39:04 +0530

    ..

--- runDocker.sh
+++ runDocker.sh
@@ -5,4 +5,4 @@ set -e
 # Build the docker image
 IMAGE="minor-api-server"
 
-docker run --rm -d -p 3000:3000 -v $(pwd):/app --name minor-api-server $IMAGE                
+docker run --rm -it -p 3000:3000 -v $(pwd):/app --name minor-api-server $IMAGE                
